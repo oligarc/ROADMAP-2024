09e8579d473d67e735eba424e824b7af0a0ce495
oligarc <oliver25399@gmail.com>
2024-08-24 19:36:35 +0200

    tercer_commit

--- #03-contactos-CRUD/main.js
+++ #03-contactos-CRUD/main.js
@@ -1,8 +1,6 @@
 import { Contacto } from "./contactos.js";
 import { mostrarAgendaCompleta,ingresarContacto,borrarContacto,borrarAgendaCompleta,mostrarMenu,pedirOpcion,buscarContacto,actualizarContacto,salir } from "./funciones.js";
 
-
-
 let setPrueba = new Set();
 
 pedirOpcion()
