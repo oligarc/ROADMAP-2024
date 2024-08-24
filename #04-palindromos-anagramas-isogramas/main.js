353552af7e1ed76754db131689bd63532d13861d
oligarc <oliver25399@gmail.com>
2024-08-24 21:17:36 +0200

    commit_final_main

--- #04-palindromos-anagramas-isogramas/main.js
+++ #04-palindromos-anagramas-isogramas/main.js
@@ -1,2 +1,4 @@
-import { palabraPalindroma,palabraPalindromaEntradaUsuario,sonAnagramas,sonAnagramasPedidoAlUsuario } from "./funciones";
-
+import { palabraPalindroma,palabraPalindromaEntradaUsuario,sonAnagramas,sonAnagramasPedidoAlUsuario,esIsograma,esIsogramaPedidoAlUsuario } from "./funciones";
+ 
+//Aquí pruebas todas las funciones.
+//Están probadas desde funciones.js y van finas
