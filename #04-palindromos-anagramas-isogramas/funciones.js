a2987f020f1b2090951ee6bab571592080c2516b
oligarc <oliver25399@gmail.com>
2024-08-24 21:17:22 +0200

    commit_isogramas

--- #04-palindromos-anagramas-isogramas/funciones.js
+++ #04-palindromos-anagramas-isogramas/funciones.js
@@ -98,4 +98,69 @@ export function sonAnagramasPedidoAlUsuario(){
     })
 }
 
+//Un isograma es una palabra o frase en la que cada letra aparece el mismo número de veces
+
+export function esIsograma(palabra){
+
+    let contadorLetras={}; //Objeto vacío que almacena cada letra como clave y número de veces que aparece como valor
+    for(let i=0;i<palabra.length;i++){
+
+        let letra = palabra[i].toLowerCase();
+
+        //Si la letra ya existe, incrementamos
+        if (contadorLetras[letra]) {
+            contadorLetras[letra]++;
+        } else {
+            // Si no está, la inicializamos con 1
+            contadorLetras[letra] = 1;
+        }
+    }
+
+    let ocurrenciasReferencia = contadorLetras[palabra[0].toLowerCase()]; //Tomas como referencia el numero de ocurrencias de la primera letra
+
+    // Verificamos si todas las letras tienen el mismo número de ocurrencias
+    for (let letra in contadorLetras) {
+        if (contadorLetras[letra] !== ocurrenciasReferencia) {
+            return false; // Si alguna letra tiene diferente número de ocurrencias, no es un isograma
+        }
+    }
+
+    return true; // Si todas las letras tienen el mismo número de ocurrencias, es un isograma
+}
+
+export function esIsogramaPedidoAlUsuario(){
+
+    let esIsograma=true;
+    rl.question("Ingrese la palabra o frase que quiera evaluar: ", (palabra) =>{
+
+        let contadorLetras = {};
+
+        for(let i=0;i<palabra.length;i++){
+            let letra = palabra[i].toLowerCase();
+
+            if(contadorLetras[letra]){
+                contadorLetras[letra]++;
+            }else{
+                contadorLetras[letra] = 1;
+            }
+        }
+
+        let ocurrenciasReferencia = contadorLetras[palabra[0].toLowerCase()];
+        for (let letra in contadorLetras) {
+            if (contadorLetras[letra] !== ocurrenciasReferencia) {
+                esIsograma=false
+                break;
+            }
+        }
+
+        if(!esIsograma){
+            console.log(`La palabra ${palabra} no es un isograma.`)
+        }else{
+            console.log(`La palabra ${palabra} es un isograma.`)
+        }
+
+    })
+}
+
+esIsogramaPedidoAlUsuario()
 
