2228f02be273d4490e356c5f19ee4ea434a98d53
oligarc <oliver25399@gmail.com>
2024-08-26 11:25:51 +0200

    segundo_commit

---
+++ #07-pilas-y-colas/pila.js
@@ -0,0 +1,5 @@
+
+//Programa que simule mecanismo adelante/atrás de diversas webs.
+//Usuario podrá elegir qué hacer, añadir url, ir adelante, ir para atrás o salir del proceso
+//Podría hacer un while true y un switch con opciones (una opción salir que salga del proceso)
+//Continuar después de trabajar
