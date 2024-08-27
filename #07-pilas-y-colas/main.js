d2beae87882acf0eee9bc3e0f168b9e98eae2618
oligarc <oliver25399@gmail.com>
2024-08-27 10:38:32 +0200

    main_y_package

---
+++ #07-pilas-y-colas/main.js
@@ -0,0 +1,7 @@
+import { salir, aniadirURL, irAdelante, irHaciaDetras, mostrarOpciones,rl,pedirOpciones } from "./pila.js";
+import readline from 'readline';
+
+
+let stack = [];
+
+pedirOpciones();
