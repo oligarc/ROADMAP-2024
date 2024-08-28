e0a6874ee7386a8f699b448b21cacf9baf97b017
oligarc <oliver25399@gmail.com>
2024-08-28 11:13:44 +0200

    final_commit_main

--- #08-clases/main.js
+++ #08-clases/main.js
@@ -1,4 +1,5 @@
 import { Impresora } from "./impresora.js";
+import { Pila } from "./pila.js";
 
 let impresora1 = new Impresora("TurboClean3000",true,400,500);
 impresora1.aniadirACola("leoMessi")
@@ -11,3 +12,14 @@ impresora1.aniadirACola("andresIniesta")
 impresora1.imprimirTodaLaCola()
 
 console.log(impresora1.getFoliosDisponibles())
+
+let pila1 = new Pila()
+pila1.apilarElemento("soad")
+pila1.apilarElemento("rammstein")
+pila1.apilarElemento("muse")
+pila1.apilarElemento("metallica")
+
+let pruebaStack = pila1.elementosRetornados()
+for (let element of pruebaStack) {
+    console.log(element)
+}
