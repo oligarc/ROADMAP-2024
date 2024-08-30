222778696fac84cfeb59575c3cc4b47f438e5e60
oligarc <oliver25399@gmail.com>
2024-08-30 11:19:01 +0200

    commit_para_programa_complejo

--- #11-manejo-de-ficheros/main.js
+++ #11-manejo-de-ficheros/main.js
@@ -15,6 +15,22 @@ import { escribirArchivo,crearArchivo,leerArchivo,borrarArchivo } from "./funcio
 
 const filePath = join('C:', 'Users', 'Oliver', 'Downloads', 'texto.txt') //esto es otra opción para pasar la ruta
 
+crearArchivo(filePath,"texto.txt")
+leerArchivo(filePath)
+
+//Inicio programa complicado
+
+/*Desarrolla un programa de gestión de ventas que almacena sus datos en un 
+ * archivo .txt.
+ * - Cada producto se guarda en una línea del archivo de la siguiente manera:
+ *   [nombre_producto], [cantidad_vendida], [precio].
+ * - Siguiendo ese formato, y mediante terminal, debe permitir añadir, consultar,
+ *   actualizar, eliminar productos y salir.
+ * - También debe poseer opciones para calcular la venta total y por producto.
+ * - La opción salir borra el .txt.
+ * 
+ * */
+
 
 
 
